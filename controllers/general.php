19a5456ca16f8465e1bd4474c8065178e03d4e2a
William Stam <awstam@gmail.com>
2012-05-20 19:05:09 +0200

    Setting up the app environment

--- controllers/general.php
+++ controllers/general.php
@@ -55,8 +55,19 @@ class general {
 			$t = file_get_contents($base . $file);
 		} else {
 			$files = array(
+				"_js/libs/jquery-ui.js",
 				"_js/libs/bootstrap.min.js",
+				"_js/plugins/bootstrap-datepicker.js",
+				"_js/plugins/date.js",
+				"_js/plugins/jquery.daterangepicker.js",
+				"_js/plugins/jquery.mousewheel.js",
+				"_js/plugins/mwheelIntent.js",
+				"_js/plugins/jquery.jscrollpane.js",
+				"_js/plugins/jquery.jqote2.js",
+				"_js/plugins/jquery.ba-bbq.js",
 				"_js/plugins/jquery.cookie.js",
+				"_js/plugins/jquery.scrollto.min.js", // ------ //
+
 
 
 
--- index.php
+++ index.php
@@ -47,6 +47,7 @@ $app->set('UI', 'ui/');
 $app->set('TEMP', 'tmp/');
 $cfg = array(
 	"db"=>array(
+		"host"=>"localhost",
 		"username"=>"",
 		"password"=>"",
 		"database"=>""
@@ -56,10 +57,6 @@ require_once('config.inc.php');
 
 
 $app->set('DB', new DB('mysql:host=' . $cfg['db']['database'] . ';dbname=' . $cfg['db']['database'], $cfg['db']['username'], $cfg['db']['password']));
-$app->set("GOOGLE_ANALYTICS", $cfg['GOOGLE_ANALYTICS']);
-
-
-
 
 ob_start();
 
@@ -72,24 +69,35 @@ $app->route('GET /min/js*', 'general->js_min', 0);
 
 $app->route('GET|POST /', 'controllers\tasks->page');
 
-$app->run();
 
+
+
+
+
+$app->run();
 $GLOBALS["render"] = ob_get_contents();
 ob_end_clean();
-
-
 $totaltime = $pageExecute->stop("Page Execute");
 $GLOBALS["output"]['timer'] = $GLOBALS['timer'];
-$GLOBALS["output"]['page'] = array(
-	"page"=> $_SERVER['REQUEST_URI'],
-	"time"=> $totaltime
-);
+$GLOBALS["output"]['page'] = array("page" => $_SERVER['REQUEST_URI'], "time" => $totaltime);
 
-
-
-
-
-$app->run();
-
+//ob_start("ob_gzhandler");
+if ((isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') || F3::get("showjson")) {
+	header("Content-Type: application/json");
+	exit(json_encode($GLOBALS["output"]));
+} else {
+	if (F3::get("__runTemplate")) {
+		;
+		$timersbottom = '
+					<script type="text/javascript">
+				       updatetimerlist(' . json_encode($GLOBALS["output"]) . ');
+					</script>
+				';
+		echo str_replace("</body>", $timersbottom . '</body>', $GLOBALS["render"]);
+	} else {
+		header("Content-Type: application/json");
+		echo json_encode($GLOBALS["output"]);
+	}
+}
 
 ?>
